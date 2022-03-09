c37902e8bdfac800dda6e470fa5a8980659e8a18
Dmitrii Teterin <Dmitrii_Teterin@epam.com>
2022-03-09 20:09:31 +0300

    lab 2 done

--- lab2/2_Teterin_8373.c
+++ lab2/2_Teterin_8373.c
@@ -1,5 +1,19 @@
+/*
+* Выполнил: Тетерин Дмитрий, 8373
+* Задание: 2. «Вызов лифта»
+* Дата выполнения: 09.03.2022
+* Скрипт для компиляции и запуска программы:
+* gcc -pthread 2_Teterin_8373.с 
+* ./a.out
+*/
+// ------------------------------------------- // 
+/*
+ * Общее описание программы:
+ * Данная программа реализует модель работы автомата, управляющим лифтом
+ * при его вызове с некоторого этажа. Для функционирования модели используются сигналы.
+ */
+// ------------------------------------------- //
 #include <stdio.h>
-#include <stdbool.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
@@ -18,38 +32,110 @@
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
-struct arg_struct{
+int state = THIS_FLOOR;
+
+struct arg_struct
+{
     sigset_t set;
 };
 
-void catch_up(int signo) {
-    printf("up signal with #%d\n", signo);
+void move(int direction)
+{
+    if (direction == Up)
+    {
+        int n = 3;
+        for (int i = 1; i <= n; i++)
+        {
+            printf("*** GO UP\n");
+            usleep(500000);
+        }
+        state = TOP;
+        printf("state %d\n", state);
+    }
+    if (direction == Down)
+    {
+        int n = 3;
+        for (int i = 1; i <= n; i++)
+        {
+            printf("*** GO DOWN\n");
+            usleep(500000);
+        }
+        state = Down;
+        printf("state %d\n", state);
+    }
 }
 
-void catch_down(int signo, siginfo_t* info, void* context) {
-    printf("down signal with #%d\n", signo);
-}
-
-void catch(int signo, siginfo_t* info, void* context) {
-    printf("signal with #%d\n", signo);
+void lift_control(int signo, siginfo_t *info, void *nk)
+{
+    switch (state)
+    {
+    case THIS_FLOOR:
+        if (signo == Up)
+        {
+            printf("This Floor, door is opened \n");
+            sleep(1);
+            move(Up);
+            state = TOP;
+        }
+        if (signo == Down)
+        {
+            printf("This Floor, door is opened \n");
+            sleep(1);
+            move(Down);
+            state = BOTTOM;
+        }
+        break;
+    case TOP:
+        if (signo == Up)
+        {
+            move(Down);
+            printf("This Floor, door is opened \n");
+            sleep(1);
+            move(Up);
+            state = TOP;
+        }
+        if (signo == Down)
+        {
+            move(Down);
+            printf("This Floor, door is opened \n");
+            sleep(1);
+            move(Down);
+            state = BOTTOM;
+        }
+        break;
+    case BOTTOM:
+        if (signo == Up)
+        {
+            move(Up);
+            printf("This Floor, door is opened \n");
+            sleep(1);
+            move(Up);
+            state = TOP;
+        }
+        if (signo == Down)
+        {
+            move(Up);
+            printf("This Floor, door is opened \n");
+            sleep(1);
+            move(Down);
+            state = BOTTOM;
+        }
+        break;
+    }
+    return;
 }
 
 void *push_button(void *args)
 {
-    struct arg_struct *set = (struct arg_struct*)args;
+    struct arg_struct *set = (struct arg_struct *)args;
     char ch;
-    //sigaction init here . . .
     struct sigaction act;
-    struct sigaction actdown;
-    act.sa_sigaction = &catch;
+    act.sa_sigaction = &lift_control;
     act.sa_flags = SA_SIGINFO;
     act.sa_mask = set->set;
     sigaction(Up, &act, NULL);
-    // sigprocmask(SIG_UNBLOCK, &set->set, NULL);
-    actdown.sa_sigaction = &catch_down;
-    actdown.sa_flags = SA_SIGINFO;
-    actdown.sa_mask = set->set;
-    sigaction(Down, &actdown, NULL);
+    sigaction(Down, &act, NULL);
+
     pid_t pid = getpid();
     while (ch != 'q')
     {
@@ -60,53 +146,23 @@ void *push_button(void *args)
         {
             siginfo_t info;
             info.si_value.sival_int = 222;
-            int val = sigqueue(pid,Up, info.si_value);
-            printf("pid:%d, val:%d, err:%d\n",pid,val,errno);
-            printf("PUSH UP BUTTON\n");
-        break;
+            sigqueue(pid, Up, info.si_value);
+            break;
         }
         case 'd':
         {
             siginfo_t info;
             info.si_value.sival_int = 223;
-            int val = sigqueue(pid,Down, info.si_value);
-            printf("pid:%d, val:%d, err:%d\n",pid,val,errno);
-            printf("PUSH DOWN BUTTON\n");
-        break;
+            sigqueue(pid, Down, info.si_value);
+            break;
         }
         case 'q':
         {
-        break;
+            break;
         }
         };
     }
     return EXIT_SUCCESS;
-}
-
-void lift_control(int signo, siginfo_t *info, void *nk)
-{
-    int state;
-    //TODO: REPLACE WITH REAL LIFT STATE
-    switch (state)
-    {
-    case THIS_FLOOR:
-        if (signo == Up)
-        {
-            //handle
-        }
-        if (signo == Down)
-        {
-            //handle
-        }
-        break;
-    case TOP:
-        //handle
-        break;
-    case BOTTOM:
-        //handle
-        break;
-    }
-    return;
 }
 
 int main(int argc, char *argv[])
@@ -120,8 +176,50 @@ int main(int argc, char *argv[])
     sigaddset(&set, Up);
     sigaddset(&set, Down);
     pthread_sigmask(SIG_UNBLOCK, &set, NULL);
-    pthread_create(&t, NULL, &push_button, (void*)&args);
+    pthread_create(&t, NULL, &push_button, (void *)&args);
     pthread_join(t, NULL);
     printf("Finish\n");
     return EXIT_SUCCESS;
 }
+// ------------------------------------------- //
+/*
+Start
+u
+This Floor, door is opened 
+*** GO UP
+*** GO UP
+*** GO UP
+state 0
+d
+*** GO DOWN
+*** GO DOWN
+*** GO DOWN
+state 49
+This Floor, door is opened 
+*** GO DOWN
+*** GO DOWN
+*** GO DOWN
+state 49
+u
+*** GO UP
+*** GO UP
+*** GO UP
+state 0
+This Floor, door is opened 
+*** GO UP
+*** GO UP
+*** GO UP
+state 0
+u
+*** GO DOWN
+*** GO DOWN
+*** GO DOWN
+state 49
+This Floor, door is opened 
+*** GO UP
+*** GO UP
+*** GO UP
+state 0
+q
+Finish
+*/
