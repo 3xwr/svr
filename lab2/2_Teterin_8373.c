762f343a331466cd933f24575fef68da41c24c5b
Dmitrii Teterin <Dmitrii_Teterin@epam.com>
2022-03-09 18:49:06 +0300

    down works but needs changes

--- lab2/2_Teterin_8373.c
+++ lab2/2_Teterin_8373.c
@@ -6,6 +6,7 @@
 #include <time.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <errno.h>
 
 #define TOP 0
 #define BOTTOM 1
@@ -17,7 +18,9 @@
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
-sigset_t set;
+struct arg_struct{
+    sigset_t set;
+};
 
 void catch_up(int signo) {
     printf("up signal with #%d\n", signo);
@@ -33,13 +36,20 @@ void catch(int signo, siginfo_t* info, void* context) {
 
 void *push_button(void *args)
 {
+    struct arg_struct *set = (struct arg_struct*)args;
     char ch;
     //sigaction init here . . .
     struct sigaction act;
+    struct sigaction actdown;
     act.sa_sigaction = &catch;
     act.sa_flags = SA_SIGINFO;
-    act.sa_mask = set;
+    act.sa_mask = set->set;
     sigaction(Up, &act, NULL);
+    sigprocmask(SIG_UNBLOCK, &set->set, NULL);
+    actdown.sa_handler = &catch_down;
+    actdown.sa_flags = 0;
+    actdown.sa_mask = set->set;
+    sigaction(Down, &actdown, NULL);
     while (ch != 'q')
     {
         ch = getchar();
@@ -50,6 +60,7 @@ void *push_button(void *args)
             siginfo_t info;
             info.si_value.sival_int = 222;
             sigqueue(0,Up, info.si_value);
+            printf("%d",errno);
             printf("PUSH UP BUTTON\n");
         break;
         }
@@ -96,13 +107,16 @@ void lift_control(int signo, siginfo_t *info, void *nk)
 
 int main(int argc, char *argv[])
 {
+    sigset_t set;
+    struct arg_struct args;
+    args.set = set;
     pthread_t t;
     printf("Start\n");
     sigemptyset(&set);
     sigaddset(&set, Up);
     sigaddset(&set, Down);
-    pthread_sigmask(SIG_BLOCK, &set, NULL);
-    pthread_create(&t, NULL, &push_button, NULL);
+    pthread_sigmask(SIG_UNBLOCK, &set, NULL);
+    pthread_create(&t, NULL, &push_button, (void*)&args);
     pthread_join(t, NULL);
     printf("Finish\n");
     return EXIT_SUCCESS;
